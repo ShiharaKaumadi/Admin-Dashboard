7a853c859b9626d1ade2381772eadb9bb36ff48e
ShiharaKaumadi <eshani9544@gmail.com>
2023-06-01 18:01:23 +0530

    Completed Dashboard UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,258 @@ function closeSidebar(){
         sidebarOpen=false;/*It updates the sidebarOpen variable to false.*/
     }
 }
+
+/*charts
+bar chart*/
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+    if(!sidebarOpen) {
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true;
+    }
+}
+
+function closeSidebar() {
+    if(sidebarOpen) {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+    series: [{
+        data: [20000,30000,50000,15000,25000,41000,12000,19000,20000,45000,25000,30000],
+        name: "Income",
+    }],
+    chart: {
+        type: "bar",
+        background: "transparent",
+        height: 350,
+        toolbar: {
+            show: false,
+        },
+    },
+    colors: [
+        "#6d7ca8",
+        "#9f7272",
+        "#68966a",
+        "#a98a73",
+        "#a57da9",
+    ],
+    plotOptions: {
+        bar: {
+            distributed: true,
+            borderRadius: 3,
+            horizontal: false,
+            columnWidth: "40%",
+        }
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    fill: {
+        opacity: 1,
+    },
+    grid: {
+        borderColor: "#55596e",
+        yaxis: {
+            lines: {
+                show: true,
+            },
+        },
+        xaxis: {
+            lines: {
+                show: true,
+            },
+        },
+    },
+    legend: {
+        labels: {
+            colors: "#f5f7ff",
+        },
+        show: true,
+        position: "top",
+    },
+    stroke: {
+        colors: ["transparent"],
+        show: true,
+        width: 2
+    },
+    tooltip: {
+        shared: true,
+        intersect: false,
+        theme: "dark",
+    },
+    xaxis: {
+        categories: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+        title: {
+            style: {
+                color: "#f5f7ff",
+            },
+        },
+        axisBorder: {
+            show: true,
+            color: "#55596e",
+        },
+        axisTicks: {
+            show: true,
+            color: "#55596e",
+        },
+        labels: {
+            style: {
+                colors: "#f5f7ff",
+            },
+        },
+    },
+    yaxis: {
+        title: {
+            text: "Income",
+            style: {
+                color:  "#f5f7ff",
+            },
+        },
+        axisBorder: {
+            color: "#55596e",
+            show: true,
+        },
+        axisTicks: {
+            color: "#55596e",
+            show: true,
+        },
+        labels: {
+            style: {
+                colors: "#f5f7ff",
+            },
+        },
+    }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+// AREA CHART
+var areaChartOptions = {
+    series: [{
+        name: "Purchase Orders",
+        data: [31, 40, 28, 51, 42, 109, 100,40,50,70,60,30],
+    }, {
+        name: "Sales Orders",
+        data: [11, 32, 45, 32, 34, 52, 41,52,36,63,50,40],
+    }],
+    chart: {
+        type: "area",
+        background: "transparent",
+        height: 350,
+        stacked: false,
+        toolbar: {
+            show: false,
+        },
+    },
+    colors: ["#4b9370", "#935353"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct","Nov","Dec"],
+    dataLabels: {
+        enabled: false,
+    },
+    fill: {
+        gradient: {
+            opacityFrom: 0.4,
+            opacityTo: 0.1,
+            shadeIntensity: 1,
+            stops: [0, 100],
+            type: "vertical",
+        },
+        type: "gradient",
+    },
+    grid: {
+        borderColor: "#55596e",
+        yaxis: {
+            lines: {
+                show: true,
+            },
+        },
+        xaxis: {
+            lines: {
+                show: true,
+            },
+        },
+    },
+    legend: {
+        labels: {
+            colors: "#f5f7ff",
+        },
+        show: true,
+        position: "top",
+    },
+    markers: {
+        size: 6,
+        strokeColors: "#1b2635",
+        strokeWidth: 3,
+    },
+    stroke: {
+        curve: "smooth",
+    },
+    xaxis: {
+        axisBorder: {
+            color: "#55596e",
+            show: true,
+        },
+        axisTicks: {
+            color: "#55596e",
+            show: true,
+        },
+        labels: {
+            offsetY: 5,
+            style: {
+                colors: "#f5f7ff",
+            },
+        },
+    },
+    yaxis:
+        [
+            {
+                title: {
+                    text: "Purchase Orders",
+                    style: {
+                        color: "#f5f7ff",
+                    },
+                },
+                labels: {
+                    style: {
+                        colors: ["#f5f7ff"],
+                    },
+                },
+            },
+            {
+                opposite: true,
+                title: {
+                    text: "Sales Orders",
+                    style: {
+                        color:  "#f5f7ff",
+                    },
+                },
+                labels: {
+                    style: {
+                        colors: ["#f5f7ff"],
+                    },
+                },
+            },
+        ],
+    tooltip: {
+        shared: true,
+        intersect: false,
+        theme: "dark",
+    }
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
